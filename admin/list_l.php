4865f02874bf295aaf6687d5fc1fa073e6b7f7d8
Name <mon98129@gmail.com>
2023-04-05 23:54:25 +0700

    กเด

--- admin/list_l.php
+++ admin/list_l.php
@@ -96,7 +96,7 @@ include "../barcode/src/BarcodeGeneratorHTML.php";
 function barcode($code){
   
   $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
-  $border = 2;//กำหนดความหน้าของเส้น Barcode
+  $border = 1.5;//กำหนดความหน้าของเส้น Barcode
   $height = 40;//กำหนดความสูงของ Barcode
 
   return $generator->getBarcode($code , $generator::TYPE_CODE_128,$border,$height);
